dce1cc09c5ea21839c671992f0135dca7d63031b
CRISTOFER MONTERO MONTERO <208150613@castrocarazo.ac.cr>
2024-08-19 20:21:26 -0600

    Add project files.

---
+++ P6Answers_API_CrisM/Models/General.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P6Answers_API_CrisM.Models;
+
+public partial class General
+{
+    public int Idconfig { get; set; }
+
+    public int StrikeTop { get; set; }
+}
---
+++ P6Answers_API_CrisM/ModelsDTOs/UserLoginRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace P6Answers_API_CrisM.ModelsDTOs
+{
+    public class UserLoginRequest
+    {
+        public string Email { get; set; } = null!;
+        public string Password { get; set; } = null!;
+    }
+}
